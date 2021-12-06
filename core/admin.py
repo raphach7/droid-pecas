70f3dce9aac1e350e8db621f35bf4a32cbc34576
raphach7 <raphach7@hotmail.com>
2021-12-06 20:06:57 -0300

    Customizacao admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,46 @@
 from django.contrib import admin
 from core.models import Endereco, Anunciante, Contato, Demanda
+from PIL import Image
 
-admin.site.register(Endereco)
-admin.site.register(Anunciante)
-admin.site.register(Contato)
-admin.site.register(Demanda)
+class EnderecoAdmin(admin.ModelAdmin):
+  list_display = (
+    "rua",
+    "numero",
+    "complemento",
+    "bairro",
+    "cep",
+    "estado",
+    "cidade",
+  )
+  
+  search_fields = ("cep",)
+
+class AnuncianteAdmin(admin.ModelAdmin):
+  list_display = (
+    "usuario",
+  )
+  
+  search_fields = ("usuario",)
+  
+class ContatoAdmin(admin.ModelAdmin):
+  list_display = (
+    "ddd",
+    "celular"
+  )
+  
+class DemandaAdmin(admin.ModelAdmin):
+  list_display = (
+    "descricao",
+    "endereco_entrega",
+    "anunciante",
+    "status_finalizacao"
+  )
+  
+  list_filter = ['status_finalizacao']
+  search_fields = ("anunciante", "descricao",)
+
+  
+admin.site.register(Endereco, EnderecoAdmin)
+admin.site.register(Anunciante, AnuncianteAdmin)
+admin.site.register(Contato, ContatoAdmin)
+admin.site.register(Demanda, DemandaAdmin)
